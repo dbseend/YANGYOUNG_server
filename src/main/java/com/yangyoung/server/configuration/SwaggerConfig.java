fb1c3eb0f92e994afb68b5e52cb3e1389174c27c
dbseend <shyoon840@gmail.com>
2023-11-13 18:59:54 +0900

    스웨거 설정

---
+++ src/main/java/com/yangyoung/server/configuration/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.yangyoung.server.configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("양영학원")
+                .description("api 명세서")
+                .version("0.0.0");
+    }
+}
